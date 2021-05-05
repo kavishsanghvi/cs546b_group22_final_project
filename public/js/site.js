952029c9d461b3ad9207ed4ad415da34a31b5c97
Deven19 <devendra103@gmail.com>
2021-05-05 10:54:42 -0400

    added site.js

--- public/js/site.js
+++ public/js/site.js
@@ -1,43 +1,19 @@
-$("#question0").css("display", "block");
+// var verifyButton = $('.btn btn-primary');
+// function verifyStudentStatus(todoItem){
+//     todoItem.find('#verifyStatusID').on('click', function(event){
+//         event.preventDefault();
+//         var currentLink = $(this);
+//         var currentID = currentLink.data('id');
+//         console.log(currentID);
 
-
-$(".quizSubmit").submit(function (event) {
-    let qID = $(this).attr("id");
-    let id1 = $("#question" + qID);
-    let id2 = $("#question" + (Number(qID) + 1));
-    id1.css("display", "none");
-    id2.css("display", "block");
-
-    let radioName = "answerChoice" + $(this).attr("id");
-    let questionId = $(this).data("id");
-    let radioValue = $("input[name='" + radioName + "']:checked").val();
-    if (radioValue) {
-        alert("Your are a - " + radioValue);
-    }
-    if (questionId) {
-        alert(questionId);
-    }
-
-    var request = $.ajax({
-        url: "./quiz-student-update",
-        method: "POST",
-        data: {
-            "questionId": questionId,
-            "selectedAns": radioValue ? radioValue : ""
-        },
-        dataType: "json"
-    });
-
-    request.done(function (showsData) {
-        alert(showsData);
-
-        if (showsData) {} else {}
-    });
-
-    request.fail(function (jqXHR, textStatus) {
-        console.log(textStatus)
-        $("#site-error").text("Error in loading show detail.").show();
-        //alert("Request failed: " + textStatus);
-    });
-    event.preventDefault();
-})
+//         var requestConfig = {
+//             // method = 'POST',
+//             url: '/verifyStudent/' + currentID
+//         };
+//         $.ajax(requestConfig).then(function(responseMessage){
+//             var newElement = $(responseMessage);
+//             bindEventsToTodoItem(newElement);
+//             todoItem.replaceWith(newElement);
+//         });
+//     });
+// }(window.jQuery)
