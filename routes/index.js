eaf15b2593ccf85bbc5dcd660bfd54a21145f1ea
Deven19 <devendra103@gmail.com>
2021-05-09 20:22:36 -0400

    added 404 page and partial handlebar for error message

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,8 @@ const constructorMethod = (app) => {
 
 
   app.use('*', (req, res) => {
-    res.status(404).json({ error: 'Not found' });
+    //res.status(404).json({ error: 'Not found' });
+    res.render('posts/404')
   });
 };
 
