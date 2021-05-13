cf9a5df9fb5716b9657b285e0af79d16393cee0b
Deven19 <devendra103@gmail.com>
2021-05-13 18:49:56 -0400

    xss, error handling, nav issue

--- data/login.js
+++ data/login.js
@@ -1,20 +1,31 @@
 const mongoCollections = require('../config/mongoCollections');
 const usersObj = mongoCollections.users;
-const jwt = require('jsonwebtoken');
+//const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
+const utils = require('./utils');
 
 
 const login = async function login(email, password) {
+
+    let checkEmail = await utils.variableSanityCheck(email, "string", "Email");
+    if (checkEmail.result)  email = checkEmail.value
+    else throw  {"result": false,"message": "", statusCode:404, error: checkEmail.message, userData:null};
+
+    let checkPassword = await utils.variableSanityCheck(password, "string", "Password");
+    if (checkPassword.result)  password = checkPassword.value
+    else throw  {"result": false,"message": "", statusCode:404, error: checkPassword.message, userData:null};
+
+
     const localUsersObj = await usersObj();
     const loggerInUser = await localUsersObj.find({email: email}).toArray();
     if (loggerInUser.length === 0)
         return {"result": false, statusCode:404, "message": "User not found!", userData:null};
     if (loggerInUser.length > 0) {
         let checkPassword = await bcrypt.compare(password, loggerInUser[0].password);
-            if(!checkPassword) throw {"result": false,"token": token,"message": "User not found!", statusCode:404, error: "User not found!", userData:null};
+            if(!checkPassword) throw {"result": false, "message": "User not found!", statusCode:404, error: "User not found!", userData:null};
            
-        var token = jwt.sign({loggerInUser}, '3e2c5bea78f9020f7c5e2bb24ac10d8b390c2ddb9fab2560ee12c24ede61d1a7', {expiresIn: '1800s'});
-        return {"result": true,"token": token, statusCode:200, "message": "user verified..",  userData:loggerInUser[0]};
+        //var token = jwt.sign({loggerInUser}, '3e2c5bea78f9020f7c5e2bb24ac10d8b390c2ddb9fab2560ee12c24ede61d1a7', {expiresIn: '1800s'});
+        return {"result": true, statusCode:200, "message": "user verified..",  userData:loggerInUser[0]};
     }
 }
 
--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const xss = require('xss');
 const data = require("../data");
 const loginData = data.login;
+const utils = require('../data/utils');
 
 router.get('/', async (req, res) => {
     try {
-        res.render('posts/login', { title: "Fibonacci & Prime Number Checker" });
+        res.render('posts/login', {});
     } catch (e) {
         res.status(500).json({
             error: e
@@ -15,33 +17,23 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-
-        if (!req.body.email || req.body.email.trim() == "") {
-            res.status(400).render('posts/login', { error: "Please pass the email", title: "Quizmania  - Login", notFound: false })
+        let email;
+        let checkEmail = await utils.variableSanityCheck(xss(req.body.email), "string", "Email");
+        if (checkEmail.result)  email = (checkEmail.value).toLowerCase()
+        else{
+            res.status(400).render('posts/login', { error: "Incorrect user input", "message":"", title: "Quizmania  - Login", notFound: false, userData:null})
+            return;
+        } 
+        
+        let password;
+        let checkPassword = await utils.variableSanityCheck(xss(req.body.password), "string", "Password");
+        if (checkPassword.result)  password = checkPassword.value
+        else{
+            res.status(400).render('posts/login', { error: "Incorrect user input", "message":"", title: "Quizmania  - Login", notFound: false, userData:null})
             return;
         }
 
-
-        if (typeof req.body.email !== "string") {
-            res.status(400).render('posts/login', { error: "search value must be string only", title: "Quizmania  - Login", notFound: false })
-            return;
-        }
-
-        if (!req.body.password || req.body.password.trim() == "") {
-            res.status(400).render('posts/login', { error: "Please pass the search value!", title: "Quizmania  - Login",  notFound: false })
-            return;
-        }
-
-
-        if (typeof req.body.password !== "string") {
-            res.status(400).render('posts/login', { error: "search value must be string only", title: "Quizmania  - Login",  notFound: false })
-            return;
-        }
-
-        let email = req.body.email.trim();
-        let password = req.body.password.trim();
-
-        let logInUser = await loginData.login(email, password);
+        let logInUser = await loginData.login(xss(email), xss(password));
         if (logInUser && logInUser.userData!=null) {            
             req.session.user = {
                 firstName: logInUser.userData.firstName,
@@ -61,9 +53,6 @@ router.post('/', async (req, res) => {
         }
         
     } catch (e) {
-        // res.status(500).json({
-        //     error: e
-        // });
         res.status(e.statusCode?e.statusCode:500).render('posts/login', { error: e.error, title: "Quizmania  - Login",  notFound: false })
     }
 });
